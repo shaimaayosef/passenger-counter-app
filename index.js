791065e311b196dd7e9be5762e62843e51109191
shaimaayosef <shimaa.sayed8888@gmail.com>
2022-03-04 22:06:54 +0200

    updating sum button

--- index.js
+++ index.js
@@ -2,17 +2,23 @@
 let count = 0
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
+let sum = document.getElementById("sum")
 
-console.log(countEl)
+
 
 function increment (){
     count = count+1
     countEl.innerText = count
 }
 
+let all = []
 function save(){
+   all.push(count)
    let countStr = count + " - "
+   let sumEnt = all.reduce((acc,el)=>acc+el,0)
    saveEl.textContent += countStr
    count = 0
    countEl.innerText = 0
+   sum.textContent = "sum of entries: " + sumEnt
+   console.log(sumEnt)
 }
